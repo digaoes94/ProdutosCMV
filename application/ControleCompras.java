da7ad97f6dcab17790ad5aad668bad0498e8c585
Rodrigo Araujo <rasch.dev@gmail.com>
2024-02-09 02:58:42 -0300

    cada dia um tropeço

--- application/ControleCompras.java
+++ application/ControleCompras.java
@@ -1,6 +1,7 @@
 package application;
 
 import java.util.Scanner;
+import java.util.Locale;
 
 import controller.RegistroCompra;
 
@@ -8,16 +9,20 @@ public class ControleCompras {
 	//  System.out.println("");
 	public static Scanner scan = new Scanner(System.in);
 	public static RegistroCompra registro = new RegistroCompra();
+	public static Locale portugues = new Locale("pt", "BR");
 	
+	//  PRINCIPAL
 	public static void main(String[] args) {
 		boolean run = true;
 		
+		System.out.println("Sistema - Controle de Compra de Mercadoria para Revenda\n");
 		do {
 			run = Menu();
 		} while (run == true);
-
 	}
 	
+	
+	//  CONTINUAÇÃO
 	public static boolean Menu() {
 		boolean continuar = true;
 		int escolha = 0;
@@ -33,25 +38,57 @@ public class ControleCompras {
 		
 		switch (escolha) {
 			case 1:
-				registro.AdicionarProduto();
+				int quantidadeProdutos = 0;
+				
+				System.out.println("Quantos produtos serão adicionados?");
+				quantidadeProdutos = scan.nextInt();
+				
+				for (int a = 1; a <= quantidadeProdutos; a++) {
+					boolean imp = false;
+					String importado = "";
+					
+					System.out.println("Dados do produto n° " + a);
+					System.out.println("O produto é importado? Digite SIM ou NAO");
+					importado = scan.next().toLowerCase();
+					
+					switch (importado) {
+						case "sim":
+							imp = true;
+							registro.AdicionarProduto(imp);
+							break;
+						case "não":
+							imp = false;
+							registro.AdicionarProduto(imp);
+							break;
+					}
+				}
+				
 				continuar = true;
 				break;
+				
 			case 2:
 				registro.RemoverProduto();
 				continuar = true;
 				break;
+				
 			case 3:
-				registro.ConsultarProduto();
+				System.out.println("Qual o nome do produto a ser consultado?");
+				String consultarProduto = scan.next();
+				System.out.println(registro.ConsultarProduto(consultarProduto));
+				
 				continuar = true;
 				break;
+				
 			case 4:
 				registro.ResumoCompra();
 				continuar = true;
 				break;
+				
 			case 5:
 				System.out.println("O programa será encerrado.");
 				continuar = false;
 				break;
+				
 			default:
 				System.out.println("Escolha inválida, o programa será encerrado.");
 				continuar = false;
--- controller/RegistroCompra.java
+++ controller/RegistroCompra.java
@@ -9,88 +9,77 @@ import entities.ProdutoImportado;
 import entities.ProdutoNacional;
 
 public class RegistroCompra {
-	//  System.out.println("");
+	// System.out.println("");
 	private Scanner scan = new Scanner(System.in);
 	protected List<Produto> produtosRevenda = new ArrayList<Produto>();
-	
-	//  ADICIONAR PRODUTO
-	public void AdicionarProduto() {
-		System.out.println("Quantos produtos serão adicionados?");
-		int quantidadeProdutos = scan.nextInt();
-		
-		for (int a = 0; a < quantidadeProdutos; a++) {
-			System.out.println("O produto é importado? Digite SIM ou NÃO");
-			String importado = scan.nextLine().toLowerCase();
-			
-			Produto produto;
-			String produtoNome;
-			Double produtoPreco, icms, ipi, impostoAlfandega;
-			
-			switch (importado) {
-				case "sim":
-					System.out.println("Digite o nome do produto:");
-					produtoNome = scan.nextLine();
-					System.out.println("Digite o preço do produto:");
-					produtoPreco = scan.nextDouble();
-					System.out.println("Digite o imposto de entrada, no país, do produto:");
-					impostoAlfandega = scan.nextDouble();
-					System.out.println("Digite o ICMS do produto:");
-					icms = scan.nextDouble();
-					
-					produto = new ProdutoImportado(produtoNome, produtoPreco, icms, impostoAlfandega);
-					produtosRevenda.add(produto);
-				break;
-					
-				case "não":
-					System.out.println("Digite o nome do produto:");
-					produtoNome = scan.nextLine();
-					System.out.println("Digite o preço do produto:");
-					produtoPreco = scan.nextDouble();
-					System.out.println("Digite o imposto de entrada, no país, do produto:");
-					ipi = scan.nextDouble();
-					System.out.println("Digite o ICMS do produto:");
-					icms = scan.nextDouble();
-					
-					produto = new ProdutoNacional(produtoNome, produtoPreco, icms, ipi);
-					produtosRevenda.add(produto);
-				break;
-			}
+
+	// ADICIONAR PRODUTO
+	public void AdicionarProduto(boolean imp) {
+		String produtoNome;
+		Double produtoPreco, icms, ipi, impostoAlfandega;
+
+		if (imp == true) {
+			System.out.println("Digite o nome do produto:");
+			produtoNome = scan.next();
+			System.out.println("Digite o preço do produto:");
+			produtoPreco = scan.nextDouble();
+			System.out.println("Digite o imposto de entrada, no país, do produto:");
+			impostoAlfandega = scan.nextDouble();
+			System.out.println("Digite o ICMS do produto:");
+			icms = scan.nextDouble();
+
+			Produto produtoImp = new ProdutoImportado(produtoNome, produtoPreco, icms, impostoAlfandega);
+			produtosRevenda.add(produtoImp);
+		} 
+		if (imp == false) {
+			System.out.println("Digite o nome do produto:");
+			produtoNome = scan.next();
+			System.out.println("Digite o preço do produto:");
+			produtoPreco = scan.nextDouble();
+			System.out.println("Digite o IPI do produto:");
+			ipi = scan.nextDouble();
+			System.out.println("Digite o ICMS do produto:");
+			icms = scan.nextDouble();
+
+			Produto produtoNac = new ProdutoNacional(produtoNome, produtoPreco, icms, ipi);
+			produtosRevenda.add(produtoNac);
 		}
+
 	}
-	
-	//  REMOVER PRODUTO
+
+	// REMOVER PRODUTO
 	public void RemoverProduto() {
 		System.out.println("Qual o nome do produto a ser removido?");
-		String removerProduto = scan.nextLine();
-		
+		String removerProduto = scan.next();
+
 		for (Produto prod : produtosRevenda) {
 			if (prod.getProdutoNome() == removerProduto) {
 				produtosRevenda.remove(prod);
 			}
 		}
 	}
-	
-	//  CONSULTAR PRODUTO
-	public void ConsultarProduto() {
-		System.out.println("Qual o nome do produto a ser consultado?");
-		String consultarProduto = scan.nextLine();
-		
+
+	// CONSULTAR PRODUTO
+	public String ConsultarProduto(String consultarProduto) {
+		String retorna = "";
+
 		for (Produto prod : produtosRevenda) {
 			if (prod.getProdutoNome() == consultarProduto) {
-				System.out.println(prod.toString());
+				retorna = prod.toString();
 			}
 		}
+		return retorna;
 	}
-	
-	//  RESUMO DA COMPRA ATÉ O MOMENTO
+
+	// RESUMO DA COMPRA ATÉ O MOMENTO
 	public void ResumoCompra() {
 		double valorTotal = 0;
-		
+
 		for (Produto prod : produtosRevenda) {
 			valorTotal = valorTotal + prod.PrecoFinal();
 			System.out.println(prod.toString());
 		}
-		
-		System.out.println("Valor final da CMV: R$ "+ String.format("%.2f", valorTotal));
+
+		System.out.println("Valor final da CMV: R$ " + String.format("%.2f", valorTotal));
 	}
 }
